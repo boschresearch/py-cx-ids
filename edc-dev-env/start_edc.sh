caf0bc40d2c36d566f2758c8ac2f2069ca4b78c8
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-02-08 10:51:38 +0100

    Make sure we delete volumes also for EDC instance on startup
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- edc-dev-env/start_edc.sh
+++ edc-dev-env/start_edc.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+docker-compose -f docker-compose.yaml -f docker-compose-api-wrapper.yaml down --volumes
 docker-compose -f docker-compose.yaml -f docker-compose-api-wrapper.yaml up --force-recreate provider-data-plane provider-control-plane consumer-data-plane consumer-control-plane api-wrapper
