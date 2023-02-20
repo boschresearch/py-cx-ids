56f3744695236fe3562b09d1e5277264c5473033
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-02-20 08:02:56 +0100

    Add path related settings
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/edc/settings.py
+++ pycxids/edc/settings.py
@@ -12,6 +12,10 @@ assert PROVIDER_EDC_BASE_URL
 PROVIDER_EDC_API_KEY = os.getenv('PROVIDER_EDC_API_KEY', 'dontuseinpublic')
 assert PROVIDER_EDC_API_KEY
 
+IDS_PATH = os.getenv('IDS_PATH', '/api/v1/ids/data')
+PROVIDER_IDS_BASE_URL = os.getenv('PROVIDER_IDS_BASE_URL', 'http://provider-control-plane:8282')
+PROVIDER_IDS_ENDPOINT = f"{PROVIDER_IDS_BASE_URL}{IDS_PATH}"
+
 # consumer side
 CONSUMER_EDC_BASE_URL = os.getenv('CONSUMER_EDC_BASE_URL', 'http://consumer-control-plane:9193/api/v1/data')
 assert CONSUMER_EDC_BASE_URL
@@ -31,3 +35,5 @@ API_WRAPPER_PASSWORD = os.getenv('API_WRAPPER_PASSWORD', 'somepassword')
 assert API_WRAPPER_PASSWORD
 
 NR_OF_ASSETS = int(os.getenv('NR_OF_ASSETS', '1'))
+
+DAPS_JWKS = os.getenv('DAPS_JWKS', 'http://daps-mock:8000/.well-known/jwks.json')
