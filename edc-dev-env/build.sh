b5067590a90bf8d68c61cf002d661508e6af567d
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-02-27 14:32:51 +0100

    build: Easier to pass build instructions
    e.g.
    ./build.sh --no-cache daps-mock
    
    The build.sh script just puts together all the docker-compose files
    
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- edc-dev-env/build.sh
+++ edc-dev-env/build.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-docker-compose -f docker-compose.yaml -f docker-compose-api-wrapper.yaml -f docker-compose-infrastructure.yaml build
+docker-compose \
+    -f docker-compose.yaml \
+    -f docker-compose-api-wrapper.yaml \
+    -f docker-compose-infrastructure.yaml \
+    -f docker-compose-helpers.yaml \
+     build "$@"
