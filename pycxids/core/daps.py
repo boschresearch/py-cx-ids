27b0d0581b3207558b359488147f6803df3929b6
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-02-27 11:37:01 +0100

    daps: Leave 'aud' empty of 'audience' is not given or empty string
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/core/daps.py
+++ pycxids/core/daps.py
@@ -16,11 +16,11 @@ from pycxids.core.settings import settings
 MINIMUM_TOKEN_VALIDITY_SECONDS = 10
 _last_daps_token = None
 
-def get_daps_access_token(audience: str):
+def get_daps_access_token(audience: str = ''):
     daps = get_daps_token(audience=audience)
     return daps['access_token']
 
-def get_daps_token(audience: str):
+def get_daps_token(audience: str = ''):
     """
     audience:   Typically the endpoint for which we request the token.
                 If not given, we use the (old) env var
@@ -30,9 +30,6 @@ def get_daps_token(audience: str):
     # TODO: Fix this! We need to check for which endpoint before we return an existing, still valid token!
     #if _daps_token_still_valid(_last_daps_token):
     #    return _last_daps_token
-
-    if not audience:
-        audience = CONSUMER_CONNECTOR_URL
 
     daps_claim_set = {
         "@context": "https://w3id.org/idsa/contexts/context.jsonld",
@@ -54,7 +51,6 @@ def get_daps_token(audience: str):
         'scope' : SCOPE,
         'client_assertion_type' : ASSERTION_TYPE,
         'client_assertion' : client_assertion,
-        #'resource': CONSUMER_CONNECTOR_URL,
         'resource': audience,
     }
 
