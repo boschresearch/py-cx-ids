02103bef59bbcfaec1516a8583ceffd00c4e0678
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-02-28 16:55:04 +0100

    Move test_assets into pycxids package
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/edc/test_assets.py
+++ pycxids/edc/test_assets.py
@@ -14,30 +14,32 @@ from requests.auth import HTTPBasicAuth
 import pytest
 from uuid import uuid4
 
-from helper import get_number_of_elements, create_asset, create_contract_definition, create_policy, get_contract_definition
-from pycxids.edc.settings import PROVIDER_EDC_BASE_URL, API_WRAPPER_BASE_URL, API_WRAPPER_USER, API_WRAPPER_PASSWORD
-
+from pycxids.edc.api import EdcProvider
+from pycxids.edc.settings import PROVIDER_EDC_BASE_URL, PROVIDER_EDC_API_KEY, API_WRAPPER_BASE_URL, API_WRAPPER_USER, API_WRAPPER_PASSWORD
+from pycxids.edc.settings import PROVIDER_IDS_BASE_URL
 
 # actual test case
 def test_create_and_delete():
     """
     """
+    provider = EdcProvider(edc_data_managment_base_url=PROVIDER_EDC_BASE_URL, auth_key=PROVIDER_EDC_API_KEY)
+
     # get number of assets before we start - we don't require a clean instance for this test case
-    nr_of_assets = get_number_of_elements(f"{PROVIDER_EDC_BASE_URL}/assets")
-    nr_of_policies = get_number_of_elements(f"{PROVIDER_EDC_BASE_URL}/policydefinitions")
-    nr_of_contractdefinitions = get_number_of_elements(f"{PROVIDER_EDC_BASE_URL}/contractdefinitions")
+    nr_of_assets = provider.get_number_of_elements("/assets")
+    nr_of_policies = provider.get_number_of_elements("/policydefinitions")
+    nr_of_contractdefinitions = provider.get_number_of_elements("/contractdefinitions")
 
     # we create a new asset (and friends)
-    asset_id = create_asset(base_url='http://daps-mock:8000/.well-known/jwks.json')
-    policy_id = create_policy(asset_id=asset_id)
-    contract_id = create_contract_definition(policy_id=policy_id, asset_id=asset_id)
-    cd = get_contract_definition(id=contract_id)
+    asset_id = provider.create_asset(base_url='http://daps-mock:8000/.well-known/jwks.json')
+    policy_id = provider.create_policy(asset_id=asset_id)
+    contract_id = provider.create_contract_definition(policy_id=policy_id, asset_id=asset_id)
+    #cd = get_contract_definition(id=contract_id)
 
     sleep(1)
     
     auth = HTTPBasicAuth(username=API_WRAPPER_USER, password=API_WRAPPER_PASSWORD)
     params = {
-        "provider-connector-url": "http://provider-control-plane:8282",
+        "provider-connector-url": PROVIDER_IDS_BASE_URL,
     }
     url = f"{API_WRAPPER_BASE_URL}/{asset_id}/xxx"
 
