bd69bbb86a63c146b51791d2217b3ad4a8f12b17
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-03-09 21:32:37 +0100

    ptt: Generate demokeys that can be pubished
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/ptt/bin/generate_ca_demo_keys.sh
+++ pycxids/ptt/bin/generate_ca_demo_keys.sh
@@ -6,33 +6,37 @@
 # SPDX-License-Identifier: Apache-2.0
 
 
-mkdir -p .secrets
+mkdir -p ./demokeys
 
 # CA: -x509 creates a self-signed certificate. Can also be used as a CA certificate
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -subj "/CN=localhost.localdomain" \
-    -keyout .secrets/ca_1.key -out .secrets/ca_1.crt
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -subj "/CN=ca1.localdomain" \
+    -keyout ./demokeys/ca_1.key -out ./demokeys/ca_1.crt
+openssl x509 -in ./demokeys/ca_1.crt -text -noout > ./demokeys/ca_1.crt.txt
 
 # client_1: without -x509 creates the csr
-openssl req -newkey rsa:2048 -new -nodes -days 3650 -subj "/CN=localhost.localdomain" \
-    -keyout .secrets/client_1.key -out .secrets/client_1.csr
+openssl req -newkey rsa:2048 -new -nodes -subj "/CN=client1.localdomain" \
+    -keyout ./demokeys/client_1.key -out ./demokeys/client_1.csr
 
 
 # cross-sign the client certificate with the CA certificate/key
-openssl x509 -CA .secrets/ca_1.crt -CAkey .secrets/ca_1.key \
+openssl x509 -CA ./demokeys/ca_1.crt -CAkey ./demokeys/ca_1.key \
     -CAcreateserial -days 365 \
-    -req -in .secrets/client_1.csr -out .secrets/client_1.crt
+    -req -in ./demokeys/client_1.csr -out ./demokeys/client_1.crt
+openssl x509 -in ./demokeys/client_1.crt -text -noout > ./demokeys/client_1.crt.txt
 
-
-# openssl req -in .secrets/client_1.csr  -text
-# openssl x509 -in .secrets/client_1.crt -text
+# openssl verify -verbose -CAfile ./demokeys/ca_1.crt ./demokeys/client_1.crt
+# openssl req -in ./demokeys/client_1.csr  -text
+# openssl x509 -in ./demokeys/client_1.crt -text
 
 # now a second CA and client_2
-openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -subj "/CN=localhost.localdomain" \
-    -keyout .secrets/ca_2.key -out .secrets/ca_2.crt
+openssl req -newkey rsa:2048 -new -nodes -x509 -days 3650 -subj "/CN=ca2.localdomain" \
+    -keyout ./demokeys/ca_2.key -out ./demokeys/ca_2.crt
+openssl x509 -in ./demokeys/ca_2.crt -text -noout > ./demokeys/ca_2.crt.txt
 
-openssl req -newkey rsa:2048 -new -nodes -days 3650 -subj "/CN=localhost.localdomain" \
-    -keyout .secrets/client_2.key -out .secrets/client_2.csr
+openssl req -newkey rsa:2048 -new -nodes -subj "/CN=client2.localdomain" \
+    -keyout ./demokeys/client_2.key -out ./demokeys/client_2.csr
 
-openssl x509 -CA .secrets/ca_2.crt -CAkey .secrets/ca_2.key \
+openssl x509 -CA ./demokeys/ca_2.crt -CAkey ./demokeys/ca_2.key \
     -CAcreateserial -days 365 \
-    -req -in .secrets/client_2.csr -out .secrets/client_2.crt
+    -req -in ./demokeys/client_2.csr -out ./demokeys/client_2.crt
+openssl x509 -in ./demokeys/client_2.crt -text -noout > ./demokeys/client_2.crt.txt
