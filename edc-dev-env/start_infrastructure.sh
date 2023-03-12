f516e0cf2d696fedbcfadca57f7c7ccebacaba74
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-03-12 16:04:50 +0100

    dev-env: Add webhook-service for consuemr and 'third'
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- edc-dev-env/start_infrastructure.sh
+++ edc-dev-env/start_infrastructure.sh
@@ -3,4 +3,5 @@
 docker-compose -f docker-compose-infrastructure.yaml -f docker-compose-helpers.yaml \
     down --volumes
 docker-compose -f docker-compose-infrastructure.yaml -f docker-compose-helpers.yaml \
-    up --force-recreate vault db daps-mock receiver-service third-receiver-service dummy-backend
+    up --force-recreate vault db daps-mock receiver-service third-receiver-service dummy-backend \
+    consumer-webhook-service third-webhook-service
