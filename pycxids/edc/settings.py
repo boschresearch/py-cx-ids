00de3d65d90cf5287a0ea87677fcc20c86d1e54c
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-03-19 21:27:28 +0100

    edc: Settings, add DUMMY_BACKEND
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/edc/settings.py
+++ pycxids/edc/settings.py
@@ -42,3 +42,5 @@ NR_OF_CALLS = int(os.getenv('NR_OF_CALLS', '100'))
 DAPS_JWKS = os.getenv('DAPS_JWKS', 'http://daps-mock:8000/.well-known/jwks.json')
 
 RECEIVER_SERVICE_BASE_URL = os.getenv('RECEIVER_SERVICE_BASE_URL', 'http://receiver-service:8000/transfer')
+
+DUMMY_BACKEND = os.getenv('DUMMY_BACKEND', 'http://dummy-backend:8000/returnparams')
