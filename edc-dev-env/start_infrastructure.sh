2bbdd80edebbd99ecd2dfff2b738e551cc6ed20a
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-03-29 14:03:37 +0200

    dev-env: Always start adminer to browse the database
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- edc-dev-env/start_infrastructure.sh
+++ edc-dev-env/start_infrastructure.sh
@@ -4,4 +4,4 @@ docker-compose -f docker-compose-infrastructure.yaml -f docker-compose-helpers.y
     down --volumes
 docker-compose -f docker-compose-infrastructure.yaml -f docker-compose-helpers.yaml \
     up --force-recreate vault db daps-mock receiver-service third-receiver-service dummy-backend \
-    consumer-webhook-service third-webhook-service
+    consumer-webhook-service third-webhook-service adminer
