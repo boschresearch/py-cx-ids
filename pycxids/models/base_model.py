1fec37ed8625d6576db59db9f08d3da64392d1c5
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-03-31 23:21:24 +0200

    model: Simplify the base class - I hope this doesn't break anything
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/models/base_model.py
+++ pycxids/models/base_model.py
@@ -1,21 +1,25 @@
 from pydantic import BaseModel
 from typing import Union, Optional, Callable, Any
 
+from fastapi.encoders import jsonable_encoder
 
 class MyBaseModel(BaseModel):
     """
     Base Model for pydatnic models to properly set the usage of aliases and provide a place for
     'global' configs
     """
-    def json(self, *, include: Union['AbstractSetIntStr', 'MappingIntStrAny'] = None, exclude: Union['AbstractSetIntStr', 'MappingIntStrAny'] = None, by_alias: bool = False, skip_defaults: bool = None, exclude_unset: bool = False, exclude_defaults: bool = False, exclude_none: bool = False, encoder: Optional[Callable[[Any], Any]] = None, models_as_dict: bool = True, **dumps_kwargs: Any) -> str:
+    def json(self, exclude_none=True, **kwargs):
         by_alias = True
         exclude_unset = True
-        return super().json(include=include, exclude=exclude, by_alias=by_alias, skip_defaults=skip_defaults, exclude_unset=exclude_unset, exclude_defaults=exclude_defaults, exclude_none=exclude_none, encoder=encoder, models_as_dict=models_as_dict, **dumps_kwargs)
+        result = super().dict(exclude_none=exclude_none, by_alias=by_alias, exclude_unset=exclude_unset, **kwargs)
+        json_result = jsonable_encoder(result) # converts e.g. datetime to string
+        return json_result
 
-    def dict(self, *, include: Union['AbstractSetIntStr', 'MappingIntStrAny'] = None, exclude: Union['AbstractSetIntStr', 'MappingIntStrAny'] = None, by_alias: bool = False, skip_defaults: bool = None, exclude_unset: bool = False, exclude_defaults: bool = False, exclude_none: bool = False) -> 'DictStrAny':
+    def dict(self, exclude_none=True, **kwargs):
         by_alias = True
         exclude_unset = True
-        return super().dict(include=include, exclude=exclude, by_alias=by_alias, skip_defaults=skip_defaults, exclude_unset=exclude_unset, exclude_defaults=exclude_defaults, exclude_none=exclude_none)
+        return super().dict(exclude_none=exclude_none, by_alias=by_alias, exclude_unset=exclude_unset, **kwargs)
 
     class Config:
         allow_population_by_field_name = True
+        use_enum_values = True
