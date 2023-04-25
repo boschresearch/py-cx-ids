3254347da477cd0ae30cc758bf022b1368d06a2f
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-04-25 15:27:27 +0200

    models: Fix model issue that was introduced with
    1fec37ed8625d6576db59db9f08d3da64392d1c5
    
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/models/base_model.py
+++ pycxids/models/base_model.py
@@ -8,15 +8,12 @@ class MyBaseModel(BaseModel):
     Base Model for pydatnic models to properly set the usage of aliases and provide a place for
     'global' configs
     """
-    def json(self, exclude_none=True, **kwargs):
-        by_alias = True
-        exclude_unset = True
+    def json(self, exclude_none=True, by_alias = True, exclude_unset = True, **kwargs):
         result = super().dict(exclude_none=exclude_none, by_alias=by_alias, exclude_unset=exclude_unset, **kwargs)
         json_result = jsonable_encoder(result) # converts e.g. datetime to string
         return json_result
 
-    def dict(self, exclude_none=True, **kwargs):
-        by_alias = True
+    def dict(self, exclude_none=True, by_alias = True, exclude_unset = True, **kwargs):
         exclude_unset = True
         return super().dict(exclude_none=exclude_none, by_alias=by_alias, exclude_unset=exclude_unset, **kwargs)
 
