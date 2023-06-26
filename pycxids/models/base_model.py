6a20cf08de04576078bbdc49f6a9f25b960e3de3
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-06-26 20:29:12 +0200

    base_model: Change exclude_unset=False as default!!!
    This is the desired default behavior now since we want
    the default values of the pydantic model ALWAYS.
    
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/models/base_model.py
+++ pycxids/models/base_model.py
@@ -8,12 +8,12 @@ class MyBaseModel(BaseModel):
     Base Model for pydatnic models to properly set the usage of aliases and provide a place for
     'global' configs
     """
-    def json(self, exclude_none=True, by_alias = True, exclude_unset = True, **kwargs):
+    def json(self, exclude_none=True, by_alias = True, exclude_unset = False, **kwargs):
         result = super().dict(exclude_none=exclude_none, by_alias=by_alias, exclude_unset=exclude_unset, **kwargs)
         json_result = jsonable_encoder(result) # converts e.g. datetime to string
         return json_result
 
-    def dict(self, exclude_none=True, by_alias = True, exclude_unset = True, **kwargs):
+    def dict(self, exclude_none=True, by_alias = True, exclude_unset = False, **kwargs):
         """
         Beware: exclude_unset=True also removes default values!
         """
