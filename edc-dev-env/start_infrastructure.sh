764889fea3dd244693d82518e07dc1d55f630df5
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-06-28 07:48:58 +0200

    dev-env: Disable a few services that are not required right now with 0.4.x
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- edc-dev-env/start_infrastructure.sh
+++ edc-dev-env/start_infrastructure.sh
@@ -3,5 +3,5 @@
 docker-compose -f docker-compose-infrastructure.yaml -f docker-compose-helpers.yaml \
     down --volumes
 docker-compose -f docker-compose-infrastructure.yaml -f docker-compose-helpers.yaml \
-    up --force-recreate vault db daps-mock receiver-service third-receiver-service provider-receiver-service dummy-backend \
-    consumer-webhook-service third-webhook-service adminer
+    up --force-recreate vault db daps-mock receiver-service dummy-backend \
+    consumer-webhook-service adminer
