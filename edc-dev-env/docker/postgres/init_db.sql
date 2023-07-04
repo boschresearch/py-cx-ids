7b622087d6fab988b7889be37fbb2a114c0030a6
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-07-04 08:32:04 +0200

    dev-env: Update validation endpoint and participant.id 0.4.x
    edr extension config for the database is added, but seems
    not working / caching in the DB.
    
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- edc-dev-env/docker/postgres/init_db.sql
+++ edc-dev-env/docker/postgres/init_db.sql
@@ -4,17 +4,20 @@ CREATE DATABASE "edc_provider_contractdefinition";
 CREATE DATABASE "edc_provider_contractnegotiation";
 CREATE DATABASE "edc_provider_policy";
 CREATE DATABASE "edc_provider_transferprocess";
+CREATE DATABASE "edc_provider_edrs";
 
 CREATE DATABASE "edc_consumer_asset";
 CREATE DATABASE "edc_consumer_contractdefinition";
 CREATE DATABASE "edc_consumer_contractnegotiation";
 CREATE DATABASE "edc_consumer_policy";
 CREATE DATABASE "edc_consumer_transferprocess";
+CREATE DATABASE "edc_consumer_edrs";
 
 CREATE DATABASE "edc_third_asset";
 CREATE DATABASE "edc_third_contractdefinition";
 CREATE DATABASE "edc_third_contractnegotiation";
 CREATE DATABASE "edc_third_policy";
 CREATE DATABASE "edc_third_transferprocess";
+CREATE DATABASE "edc_third_edrs";
 
-CREATE DATABASE "registry"
+CREATE DATABASE "registry";
