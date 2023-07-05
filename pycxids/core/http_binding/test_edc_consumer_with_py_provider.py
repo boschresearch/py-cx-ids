1db43cd191fc5e2740e4429fe77868f6e79c594f
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-07-05 11:16:23 +0200

    dsp: Basic test with EDC as consumer and py as provider
    Does not yet receive an EDR token. Transition of states are good.
    
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/core/http_binding/test_edc_consumer_with_py_provider.py
+++ pycxids/core/http_binding/test_edc_consumer_with_py_provider.py
@@ -15,29 +15,34 @@ import pytest
 from uuid import uuid4
 
 from pycxids.edc.api import EdcConsumer, EdcProvider
-from pycxids.edc.settings import CONSUMER_EDC_API_KEY, CONSUMER_EDC_BASE_URL, PROVIDER_EDC_BASE_URL, PROVIDER_EDC_API_KEY, API_WRAPPER_BASE_URL, API_WRAPPER_USER, API_WRAPPER_PASSWORD, PROVIDER_IDS_ENDPOINT
+from pycxids.edc.settings import CONSUMER_EDC_API_KEY, CONSUMER_EDC_BASE_URL, PROVIDER_EDC_API_KEY, API_WRAPPER_BASE_URL, API_WRAPPER_USER, API_WRAPPER_PASSWORD, PROVIDER_IDS_ENDPOINT, RECEIVER_SERVICE_BASE_URL
 from pycxids.edc.settings import PROVIDER_IDS_BASE_URL
 
 
 def test():
     """
     """
+    asset_id = 'demo_asset'
+    provider_base_url = 'http://dev:8080' # needs to be started separately
 
     consumer = EdcConsumer(
         edc_data_managment_base_url=CONSUMER_EDC_BASE_URL,
         auth_key=CONSUMER_EDC_API_KEY,
-        token_receiver_service_base_url='http://receiver-service:8000/transfer'
+        token_receiver_service_base_url=RECEIVER_SERVICE_BASE_URL,
         )
-
-    asset_id = 'demo_asset'
-    provider_base_url = 'http://dev:8080'
     agreement_id, transfer_id = consumer.negotiate_and_transfer(provider_ids_endpoint=provider_base_url, asset_id=asset_id)
-    #provider_edr = consumer.edr_provider_wait(transfer_id=transfer_id)
-    edrs = consumer.edr_tokens(agreement_id=agreement_id)
-    print(edrs)
 
+    # test call via CONSUMER data plane
+    consumer_edr = consumer.edr_consumer_wait(transfer_id=transfer_id)
+    consumer_data_plane_endpoint = consumer_edr.get('endpoint')
+    r = requests.get(consumer_data_plane_endpoint, headers={consumer_edr['authKey']: consumer_edr['authCode']})
+    if not r.ok:
+        print(f"{r.status_code} {r.reason} {r.content}")
+        assert False, "Could not fetch data via CONSUMER data plane"
+
+    # test call directly against the PROVIDER
+    provider_edr = consumer.edr_provider_wait(transfer_id=transfer_id)
     provider_data_plane_endpoint = provider_edr.get('baseUrl')
-
     r = requests.get(provider_data_plane_endpoint, headers={provider_edr['authKey']: provider_edr['authCode']})
     if not r.ok:
         print(f"{r.status_code} {r.reason} {r.content}")
