088f26ff1728bc5841db3ebe4a591eb1efa6775a
Matthias Binzer <matthias.binzer@de.bosch.com>
2023-07-11 22:05:05 +0200

    api: Add post query params
    Does this work in all cases without params?
    
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/utils/api.py
+++ pycxids/utils/api.py
@@ -31,11 +31,11 @@ class GeneralApi():
         j = r.json()
         return j
     
-    def post(self, path: str, data = None, json_content = True):
+    def post(self, path: str, data = None, params = None, json_content = True):
         """
         Generic API POST request
         """
-        r = requests.post(f"{self.base_url}{path}",  auth=self.basic_auth, json=data, headers=self.headers)
+        r = requests.post(f"{self.base_url}{path}",  auth=self.basic_auth, json=data, headers=self.headers, params=params)
 
         if not r.ok:
             print(f"{r.status_code} - {r.reason} - {r.content}")
