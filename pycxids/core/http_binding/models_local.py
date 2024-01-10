ea1f4c0f60df097bac21319511fbd97fd2812c06
Matthias Binzer <matthias.binzer@de.bosch.com>
2024-01-10 14:25:52 +0100

    Update storage classes for sates of transfer and negotiation
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/core/http_binding/models_local.py
+++ pycxids/core/http_binding/models_local.py
@@ -14,10 +14,14 @@ from pydantic import Field
 
 # these are the not generated models, e.g. for storage
 
+
 class NegotiationStateStore(MyBaseModel):
     id: str # provider_id
+    tenant_id: Optional[str] = Field('')
     state: Optional[NegotiationState] = Field('')
     negotiation_request_id: Optional[str] = Field('')
+    negotiation_callback_address: Optional[str] = Field()
+    verification_message_id: Optional[str] = Field('')
     process_id: Optional[str] = Field('')
     dataset_id: Optional[str] = Field('')
     agreement_id: Optional[str] = Field('')
@@ -27,7 +31,7 @@ class DataAddress(MyBaseModel):
     This is EDC specific. TODO: where to stanardize this?
     """
     field_type: Optional[str] = Field(f"{EDC_NAMESPACE}DataAddress", alias='@type') # TODO: EDC compat
-    edc_cid: Optional[str] = Field(None, alias='edc:cid')
+    #edc_cid: Optional[str] = Field(None, alias='edc:cid') # no longer used in EDC 0.5.3
     edc_type: Optional[str] = Field("EDR", alias='edc:type')
     edc_auth_code: Optional[str] = Field(None, alias='edc:authCode')
     edc_endpoint: Optional[str] = Field(None, alias='edc:endpoint')
@@ -35,10 +39,12 @@ class DataAddress(MyBaseModel):
     edc_auth_key: Optional[str] = Field(HTTP_HEADER_DEFAULT_AUTH_KEY, alias='edc:authKey')
 
 class TransferStateStore(MyBaseModel):
-    id: str
-    process_id: Optional[str] # @id from the request
+    id: str # a provider id
+    tenant_id: Optional[str] = Field('')
     state: Optional[TransferState]
-    agreement_id: Optional[str]
+    transfer_request_id: Optional[str] = Field('')
     callback_address_request: Optional[str]
+    process_id: Optional[str]
+    agreement_id: Optional[str]
     data_address: Optional[DataAddress]
 
