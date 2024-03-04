3cc18ebb280c1342ca5fddf7055420923ff554ee
Matthias Binzer <matthias.binzer@de.bosch.com>
2024-03-04 08:58:12 +0100

    test: Use edc:participantId from the offer for negotiation
    and do some more debugging, e.g. context and the selected offer
    
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/edc/test_assets.py
+++ pycxids/edc/test_assets.py
@@ -96,12 +96,24 @@ def test():
         token_receiver_service_base_url=RECEIVER_SERVICE_BASE_URL,
         )
     catalog = consumer.get_catalog(provider_ids_endpoint=PROVIDER_IDS_ENDPOINT)
+    catalog_context = catalog.get('@context')
+    with open('catalog_context.json', 'wt') as f:
+        f.write(json.dumps(catalog_context, indent=True))
     contract_offer = consumer.find_first_in_catalog(catalog=catalog, asset_id=asset_id)
     assert contract_offer, "Could not find matching offer in catalog"
+    with open('catalog_offer.json', 'wt') as f:
+        f.write(json.dumps(contract_offer, indent=True))
+
+    provider_edc_participant_id = catalog.get('edc:participantId')
+    assert provider_edc_participant_id, "Could not find edc:participantId from received catalog result"
 
     negotiated_contract = consumer.negotiate_contract_and_wait(provider_ids_endpoint=PROVIDER_IDS_ENDPOINT,
-        contract_offer=contract_offer, asset_id=asset_id, provider_participant_id=settings.PROVIDER_PARTICIPANT_ID,
+        contract_offer=contract_offer, asset_id=asset_id,
+        provider_participant_id=provider_edc_participant_id,
+        consumer_participant_id=settings.CONSUMER_PARTICIPANT_ID,
+        timeout=60
         )
+    assert negotiated_contract, "Could not negotiate contract"
     agreement_id = negotiated_contract.get('edc:contractAgreementId', '')
     print(f"agreementId: {agreement_id}")
 
