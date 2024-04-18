e5ea2cd47eadacc6d10e7c28bdc0a585f5a4edc3
Matthias Binzer <matthias.binzer@de.bosch.com>
2024-04-18 11:44:53 +0200

    Cleanup consumer test
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- pycxids/core/http_binding/test_edc_consumer_with_py_provider.py
+++ pycxids/core/http_binding/test_edc_consumer_with_py_provider.py
@@ -4,19 +4,11 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 
-
-import os
-import sys
-from time import sleep
-import json
-import requests
-from requests.auth import HTTPBasicAuth
 import pytest
-from uuid import uuid4
 
-from pycxids.edc.api import EdcConsumer, EdcProvider
-from pycxids.edc.settings import CONSUMER_EDC_API_KEY, CONSUMER_EDC_BASE_URL, PROVIDER_EDC_API_KEY, API_WRAPPER_BASE_URL, API_WRAPPER_USER, API_WRAPPER_PASSWORD, PROVIDER_IDS_ENDPOINT, RECEIVER_SERVICE_BASE_URL
-from pycxids.edc.settings import PROVIDER_IDS_BASE_URL
+from pycxids.edc.api import EdcConsumer
+from pycxids.edc.settings import CONSUMER_EDC_API_KEY, CONSUMER_EDC_BASE_URL, RECEIVER_SERVICE_BASE_URL
+from pycxids.core.settings import settings
 
 
 def test():
@@ -24,13 +16,27 @@ def test():
     """
     asset_id = 'demo_asset'
     provider_base_url = 'http://dev:8080' # needs to be started separately
+    provider_ids_endpoint = f"{provider_base_url}"
 
     consumer = EdcConsumer(
         edc_data_managment_base_url=CONSUMER_EDC_BASE_URL,
         auth_key=CONSUMER_EDC_API_KEY,
         token_receiver_service_base_url=RECEIVER_SERVICE_BASE_URL,
         )
-    agreement_id, transfer_id = consumer.negotiate_and_transfer(provider_ids_endpoint=provider_base_url, asset_id=asset_id)
+
+    catalog = consumer.get_catalog(provider_ids_endpoint=provider_ids_endpoint)
+    contract_offer = consumer.find_first_in_catalog(catalog=catalog, asset_id=asset_id)
+    assert contract_offer, "Could not find matching offer in catalog"
+
+    provider_edc_participant_id = catalog.get('edc:participantId')
+    assert provider_edc_participant_id, "Could not find edc:participantId from received catalog result"
+
+    negotiated_contract = consumer.negotiate_contract_and_wait(provider_ids_endpoint=provider_ids_endpoint,
+        contract_offer=contract_offer, asset_id=asset_id,
+        provider_participant_id=provider_edc_participant_id,
+        consumer_participant_id=settings.CONSUMER_PARTICIPANT_ID,
+        timeout=60
+        )
 
     # test call via CONSUMER data plane
     consumer_edr = consumer.edr_consumer_wait(transfer_id=transfer_id)
