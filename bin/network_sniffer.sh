05fd7629f8256a3704afb71f4d40bc61d3aae451
Matthias Binzer <matthias.binzer@de.bosch.com>
2024-05-21 21:59:25 +0200

    network sniffer script
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- bin/network_sniffer.sh
+++ bin/network_sniffer.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
-IP=`docker network inspect edc-dev-env_default | jq -r '.[0].IPAM.Config[0].Gateway'`
+# install httpry and net-tools
+DOCKER_NETWORK_NAME=edc-dev-env_default
+
+IP=`docker network inspect $DOCKER_NETWORK_NAME | jq -r '.[0].IPAM.Config[0].Gateway'`
 echo $IP
 
 INTERFACE=`ifconfig | grep -B1 $IP | head -1 | awk -F ':' '{print $1}'`
