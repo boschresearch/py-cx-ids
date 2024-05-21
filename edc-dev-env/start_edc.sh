8f3a082884261970161a4c701a15d09d4ee74360
Matthias Binzer <matthias.binzer@de.bosch.com>
2024-05-21 21:45:35 +0200

    Update edc-dev-env to 0.7.x
    with information from the migration guide and debugging.
    consumer-data-plane disabled, because not used.
    
    Still testing data in the configs. Some services need
    to be mocked to work locally.
    
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- edc-dev-env/start_edc.sh
+++ edc-dev-env/start_edc.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker-compose -f docker-compose.yaml -f docker-compose-api-wrapper.yaml down --volumes
-docker-compose -f docker-compose.yaml -f docker-compose-api-wrapper.yaml up --force-recreate \
-    provider-data-plane provider-control-plane \
-    consumer-control-plane consumer-data-plane
+docker-compose -f docker-compose.yaml up --force-recreate \
+   provider-data-plane provider-control-plane \
+   consumer-control-plane
