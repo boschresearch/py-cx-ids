e5be2b4b07372a1e203911c4274997110332bb4f
Matthias Binzer <matthias.binzer@de.bosch.com>
2025-06-06 15:30:17 +0200

    edc-dev-env: Remove unused services
    Signed-off-by: Matthias Binzer <matthias.binzer@de.bosch.com>

--- edc-dev-env/start_infrastructure.sh
+++ edc-dev-env/start_infrastructure.sh
@@ -3,5 +3,4 @@
 docker-compose -f docker-compose-infrastructure.yaml -f docker-compose-helpers.yaml \
     down --volumes
 docker-compose -f docker-compose-infrastructure.yaml -f docker-compose-helpers.yaml \
-    up --force-recreate vault db receiver-service dummy-backend \
-    adminer dsp-consumer cx-services-mocks
+    up --force-recreate vault db callback-service cx-services-mocks
